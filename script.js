89ea355d4f5320a5bf02047d040329ffb61f4e92
Anmol Alawadhi <alawadhi.anmol@gmail.com>
2024-08-07 23:10:57 +0200

    newparts
    new parts

--- script.js
+++ script.js
@@ -93,3 +93,30 @@ window.addEventListener('scroll', () => {
     gif.style.backgroundPositionY = yPos + 'px';
   });
 });
+
+const carousel = document.querySelector('.testimonial-carousel');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+carousel.addEventListener('mousedown', (e) => {
+  isDown = true;
+  startX = e.pageX - carousel.offsetLeft;
+  scrollLeft = carousel.scrollLeft;
+});
+
+carousel.addEventListener('mouseleave', () => {
+  isDown = false;
+});
+
+carousel.addEventListener('mouseup', () => {
+  isDown = false;
+});
+
+carousel.addEventListener('mousemove', (e) => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - carousel.offsetLeft;
+  const walk = (x - startX) * 2;
+  carousel.scrollLeft = scrollLeft - walk;
+});
